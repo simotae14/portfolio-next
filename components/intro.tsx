1877017543f0df2daba73c2184bc173cb4051a8e
Simone Taeggi <startae14@gmail.com>
2023-10-19 00:49:01 +0200

    add buttons in the intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,6 +4,10 @@ import Image from 'next/image'
 import React from 'react'
 import { motion } from 'framer-motion'
 import myImg from "@/public/simo.png";
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
   return (
@@ -53,6 +57,39 @@ export default function Intro() {
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.p>
+
+      <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link 
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" /></Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href="/CV.pdf" download
+        >Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition" /></a>
+
+        <a 
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/simone-taeggi/"
+          target='_blank'
+        >
+          <BsLinkedin />
+        </a>
+
+        <a 
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/simotae14"
+          target='_blank'
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   )
 }
