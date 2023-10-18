e8d46d66b81dcea4ae9b0a88d49b4dd3b17eb6a5
Simone Taeggi <startae14@gmail.com>
2023-10-19 00:08:06 +0200

    add intro text

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,7 +7,7 @@ import myImg from "@/public/simo.png";
 
 export default function Intro() {
   return (
-    <section>
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
       <div className='flex items-center justify-center'>
         <div className='relative'>
           <motion.div
@@ -43,6 +43,16 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+      <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Simone.</span> I'm a{" "}
+        <span className="font-bold">front-end developer</span> with{" "}
+        <span className="font-bold">8 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.p>
     </section>
   )
 }
